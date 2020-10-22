d244b4cec5d86575da129c402c5fdfe28b9e25d6
fakhira <fakhira.yaqub>
2020-10-22 11:11:43 +0200

    added home.js file with searh bar to start working on code

---
+++ src/components/home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./home.css";
+
+
+
+  function CreateSearchBar(){
+
+
+return (<div className="search-bar"> 
+<input type="placeholder">
+</input>
+</div>)
+
+
+}
+
+
+
+export default CreateSearchBar
